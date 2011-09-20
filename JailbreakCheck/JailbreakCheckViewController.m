05923e6ea2ab097f5d0b99f7b226b88c2ce0a47a
Seung-Un Ham <hseungun@gmail.com>
2011-09-20 11:49:34 +0900

    check if iPhone is jailbroken

--- JailbreakCheck/JailbreakCheckViewController.m
+++ JailbreakCheck/JailbreakCheckViewController.m
@@ -10,35 +10,40 @@
 
 @implementation JailbreakCheckViewController
 
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
+
+- (void)viewDidLoad {
     
-    // Release any cached data, images, etc that aren't in use.
-}
+    NSArray *jailbrokenPath = [NSArray arrayWithObjects:
+                               @"/Applications/Cydia.app",
+                               @"/Applications/RockApp.app",
+                               @"/Applications/Icy.app",
+                               @"/usr/sbin/sshd",
+                               @"/usr/bin/sshd",
+                               @"/usr/libexec/sftp-server",
+                               @"/Applications/WinterBoard.app",
+                               @"/Applications/SBSettings.app",
+                               @"/Applications/MxTube.app",
+                               @"/Applications/IntelliScreen.app",
+                               @"/Library/MobileSubstrate/DynamicLibraries/Veency.plist",
+                               @"/Applications/FakeCarrier.app",
+                               @"/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist",
+                               @"/private/var/lib/apt",
+                               @"/Applications/blackra1n.app",
+                               @"/private/var/stash",
+                               @"/private/var/mobile/Library/SBSettings/Themes",
+                               @"/System/Library/LaunchDaemons/com.ikey.bbot.plist",
+                               @"/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist",
+                               @"/private/var/tmp/cydia.log",
+                               @"/private/var/lib/cydia", nil];
+    
+    for(NSString *string in jailbrokenPath)
+        if ([[NSFileManager defaultManager] fileExistsAtPath:string])
+    NSLog(@"%@ - string",string);
+    
 
-#pragma mark - View lifecycle
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad
-{
+    
     [super viewDidLoad];
 }
-*/
 
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
 
 @end
